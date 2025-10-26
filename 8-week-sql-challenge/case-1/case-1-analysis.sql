121597d348174d4eb44708d26c79129f1394e59b
Duane Edmonds <duane.edmonds@gmail.com>
2025-10-26 00:47:52 -0700

    updated case 1 analysis

--- 8-week-sql-challenge/case-1/case-1-analysis.sql
+++ 8-week-sql-challenge/case-1/case-1-analysis.sql
@@ -19,16 +19,16 @@ select s.customer_id,
        sum(m.price) as total_spent
 from sales s
      left join menu m on s.product_id = m.product_id
-group by s.customer_id
-order by sum(m.price) desc;
+group by 1
+order by 2 desc;
 
 
 -- 2. customer days at restaurant
 select customer_id,
        count(distinct order_date) as days
 from sales
-group by customer_id
-order by customer_id;
+group by 1
+order by 1;
 
 
 -- 3. first item purchased by each customer
@@ -40,7 +40,7 @@ select distinct
         as product_name
 from sales s
      left join menu m on s.product_id = m.product_id
-order by s.customer_id;
+order by 1;
 
 
 -- 4. most purchased item and number of purchases by each customer
@@ -48,9 +48,10 @@ with most_purchased as (
     select product_id,
            count(*) as n_sold
     from sales
-    group by product_id
-    order by count(*) desc
-    limit 1)
+    group by 1
+    order by 2 desc
+    limit 1
+)
 
 select distinct
     s.customer_id,
@@ -61,6 +62,45 @@ from sales s
                        count(*) as n_purchased
                 from sales s
                 where s.product_id = (select m.product_id from most_purchased m)
-                group by s.customer_id) p
+                group by 1) p
         on s.customer_id = p.customer_id
-order by s.customer_id;
+order by 1;
+
+
+-- 5. most popular item for each customer
+with popular_ranks as (
+    select customer_id,
+           product_id,
+           count(*) as count,
+           rank() over(partition by customer_id order by count(*) desc) as pop_rank
+    from sales
+    group by 1, 2
+)
+
+select customer_id,
+       string_agg(product_id) as most_popular_items
+from popular_ranks
+where pop_rank = 1
+group by 1
+order by 1;
+
+
+-- 6. first item purchase for new members
+with product_order as (
+    select s.customer_id,
+           s.product_id,
+           rank() over(partition by s.customer_id order by order_date) as order_of_purchase
+    from sales s
+         left join members m on s.customer_id = m.customer_id
+    where s.customer_id in (select customer_id from members)
+          and s.order_date >= m.join_date
+)
+
+select customer_id,
+       product_id
+from product_order
+where order_of_purchase = 1
+order by 1;
+
+
+-- 7.
